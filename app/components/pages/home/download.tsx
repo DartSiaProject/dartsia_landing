2416d98ef86f75b508536908e4c810d06df96d37
Armel Fogue <steriamfogue@gmail.com>
2025-03-26 10:17:10 +0100

    fix: responsive ui on download section

--- app/components/pages/home/download.tsx
+++ app/components/pages/home/download.tsx
@@ -4,7 +4,7 @@ export default function Download() {
   return (
     <section className="bg-[#1DB5BE]" id="download">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-2 gap-x-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-x-8 items-center lg:items-stretch">
           <div className="my-12 py-12">
             <h2 className="text-5xl font-bold mb-2">
               Manage your renterd Server from your mobile
@@ -15,19 +15,19 @@ export default function Download() {
               the status of the Sia network from your mobile device.
             </p>
 
-            <div className="mt-8">
+            <div className="mt-8 ">
               <p className="text-lg font-semibold">Get the App</p>
               <div className="flex gap-x-4">
                 <Link to={"/"}>
                   <img
-                    className="h-8 w-auto"
+                    className="h-12 w-auto"
                     src="/assets/icons/play-store.svg"
                     alt="Logo"
                   />
                 </Link>
                 <Link to={"/"}>
                   <img
-                    className="h-8 w-auto"
+                    className="h-12 w-auto"
                     src="/assets/icons/app-store.svg"
                     alt="Logo"
                   />
@@ -37,7 +37,10 @@ export default function Download() {
           </div>
 
           {/* Mockups */}
-          <div className="grid grid-cols-2">
+          <div className="hidden md:block lg:hidden">
+            <img src="/assets/images/phone-mockup.png" alt="Mobile app" />
+          </div>
+          <div className="hidden lg:grid lg:grid-cols-2">
             <div className="relative overflow-hidden">
               <div className="absolute -top-16">
                 <img
